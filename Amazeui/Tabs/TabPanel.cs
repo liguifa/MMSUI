d437627a00930cd32ec7aa387acf7188c402d85a
liguifa <liguifa18840848462@outlook.com>
2016-04-28 22:32:10 +0800

    添加Tabs控件

---
+++ Amazeui/Tabs/TabPanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MMS.UI.Amazeui
+{
+    public class TabPanel : Control
+    {
+        public String Title
+        {
+            get { return (String)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(String), typeof(TabPanel), new PropertyMetadata(null));
+
+        public String Context
+        {
+            get { return (String)GetValue(ContextProperty); }
+            set { SetValue(ContextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContextProperty =
+            DependencyProperty.Register("Context", typeof(String), typeof(TabPanel), new PropertyMetadata(null));
+
+
+    }
+}
---
+++ Amazeui/Tabs/Tabs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace MMS.UI.Amazeui
+{
+    public class Tabs : Control, IAddChild
+    {
+        public Tabs()
+        {
+            this.Style = (Style)Application.Current.Resources["Amazeui_TabsStyle"];
+        }
+
+        public void AddChild(object value)
+        {
+            
+        }
+
+        public void AddText(string text)
+        {
+            
+        }
+    }
+}
